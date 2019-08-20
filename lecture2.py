e687912cbe24c0f6662e7783096f5bae1641c39e
Kudzai Sibanda <kdzsibanda9@gmail.com>
2019-08-20 16:24:49 +0200

    Add CHanges on a new branch

--- lecture2.py
+++ lecture2.py
@@ -2,9 +2,18 @@
 import pandas as pd
 import pandas_datareader.data as pdr
 import datetime as dt
+import matplotlib.pyplot as plt
+
 
 start_date= dt.datetime(2010,1,1)
 end_date = dt.datetime(2019,6,30)
 df = pdr.DataReader("MMM","yahoo",start_date,end_date)
 print(df.head())
 
+
+fig1 = plt.subplot2grid((7,1),(0,0),rowspan=5,colspan=1)
+fig2 = plt.subplot2grid((7,1),(5,0),rowspan=2,colspan=1,sharex=fig1)
+
+fig1.plot(df['Adj Close'])
+fig2.plot(df['Volume'])
+plt.show()
